4dd9964b93bc52dd24ea8a70008d9d2154efc9eb
marcelovr13 <aluval4803@ieselcaminas.org>
2022-02-10 20:13:25 +0100

    Figuras

--- src/Circulo.java
+++ src/Circulo.java
@@ -18,6 +18,17 @@ public class Circulo {
         System.out.println("("+ x + "," + y + ")");
     }
 
+    public boolean solapa(Punto puntoC1, Punto puntoC2, int radio1, int radio2){
+        double paso1 = ((puntoC1.getX() * puntoC1.getX() - puntoC1.getY() * puntoC1.getY()) + (puntoC2.getX() * puntoC2.getX() - puntoC2.getY() * puntoC2.getY()));
+        double resultado = Math.sqrt(paso1);
+        double sumaRadios = radio1 + radio2;
+        if (resultado < sumaRadios){
+            return true;
+        }else
+            return false;
+
+    }
+
     public double distancia(Punto puntoR, Punto puntoC){
         double resultado = ((puntoR.getX() * puntoR.getX() - puntoR.getY() * puntoR.getY()) + (puntoC.getX() * puntoC.getX() - puntoC.getY() * puntoC.getY()));
         return Math.sqrt(resultado);
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,30 @@ public class Main {
 
         Punto puntoRectangulo = new Punto(4,3);
         Punto puntoCirculo = new Punto(7,2);
+        Punto puntoCirculo2 = new Punto(8,12);
 
         Rectangulo rectangulo = new Rectangulo(5, 6, puntoRectangulo);
         System.out.println("El area del rectangulo es de: " + rectangulo.area());
 
         Circulo circulo = new Circulo(5, puntoCirculo);
         System.out.println("El area del circulo es de: " + circulo.area());
+        Circulo circulo2 = new Circulo(8, puntoCirculo2);
+
 
         System.out.println("El punto central del rectangulo es: ");
         rectangulo.centro();
         System.out.println("El punto central del circulo es: " );
         circulo.centro();
-
         System.out.println("La distancia entre el rectangulo y el circulo es de: " +  rectangulo.distancia(puntoRectangulo, puntoCirculo));
+
+
+        if (circulo.solapa(puntoCirculo, puntoCirculo2,5,8)){
+            System.out.println("Los circulos  se solapan");
+        }else{
+            System.out.println("Los circulos no se solapan");
+        }
+
+
 
 
 
